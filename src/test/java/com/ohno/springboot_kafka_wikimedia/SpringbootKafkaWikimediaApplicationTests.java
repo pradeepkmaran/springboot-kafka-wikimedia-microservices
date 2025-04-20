b47364061e4d07fa10b2ed2d022d9660455e9ebb
Pradeep KM <pradeepkm6122004@gmail.com>
2025-04-20 18:36:26 +0530

    initial commit

---
+++ src/test/java/com/ohno/springboot_kafka_wikimedia/SpringbootKafkaWikimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohno.springboot_kafka_wikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaWikimediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
