57d9d348431f597bcf59e85832c0b601bd9e6395
Pradeep KM <pradeepkm6122004@gmail.com>
2025-04-21 05:45:09 +0530

    feat: KafkaDatabaseConsumer

---
+++ springboot-kafka-wikimedia-consumer/src/main/java/com/ohno/consumer/KafkaDatabaseConsumer.java
@@ -0,0 +1,17 @@
+package com.ohno.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaDatabaseConsumer {
+    private static Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+
+    @KafkaListener(topics = "wikimedia_recent_change", groupId = "myGroup")
+    public void consume(String eventMessage) {
+        LOGGER.info(String.format("Message received -> %s", eventMessage));
+    }
+
+}
